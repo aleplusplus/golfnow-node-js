bf9098c4394bc5a09ca2b2d5e47d0df4c639038b
Alejandro Labrada Diaz <alabradadiaz@gmail.com>
2016-10-21 17:14:06 -0400

    Add get rates

--- examples/api_root.js
+++ examples/api_root.js
@@ -8,7 +8,7 @@ var golfnow = require("../index");
 
 var app = golfnow("CJUALSXXDDWRWJ3XF5FXQCGRVEQAHF1U", "]=9&[I5kmlL+]:&|>vi&");
 
-var request = app.getInvoice(19818, 123123);
+var request = app.getRate(19818, 1179, 2016, 10, 21, 18, 30);
 
 request.on('response', function(response) {
     console.log(response);
