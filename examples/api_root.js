5e84d7c248ca1d9b8ed6fbea2336d7902422282f
Alejandro Labrada Diaz <alabradadiaz@gmail.com>
2016-10-21 04:36:37 -0400

    Add get invoices

--- examples/api_root.js
+++ examples/api_root.js
@@ -8,7 +8,7 @@ var golfnow = require("../index");
 
 var app = golfnow("CJUALSXXDDWRWJ3XF5FXQCGRVEQAHF1U", "]=9&[I5kmlL+]:&|>vi&");
 
-var request = app.getCourseSummary(19818, {latitude:25.761681, longitude:-80.191788});
+var request = app.getInvoice(19818, 123123);
 
 request.on('response', function(response) {
     console.log(response);
